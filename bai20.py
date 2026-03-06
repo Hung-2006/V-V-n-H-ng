811be7d843c54b4e3812e6c03d3df15dd7423c8f
Hung-2006 <vovanhung1602@gmail.com>
2026-03-06 16:30:16 +0700

    Add electricity cost calculation based on kW usage

---
+++ bai20.py
@@ -0,0 +1,12 @@
+kw = int(input("Nhap so kW tieu thu: "))
+
+if kw <= 100:
+    tien = kw * 500
+elif kw <= 250:
+    tien = 100*500 + (kw-100)*800
+elif kw <= 350:
+    tien = 100*500 + 150*800 + (kw-250)*1000
+else:
+    tien = 100*500 + 150*800 + 100*1000 + (kw-350)*1500
+
+print("Chi phi:", tien)
